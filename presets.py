f1b3724b016fd1ed5ec7fbf66adb41110c1c7a13
waly <85253290+jackblose@users.noreply.github.com>
2021-09-01 05:10:01 +0700

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,10 +1,9 @@
 class Presets(object):
     WELCOME_TEXT = "Hai, <b>{}</b>\n<code>Saya Adalah Asisten Della Cantik\nKirim pesan kamu ke della disini👇🏻</code> "
+                   ("ᴀsᴜᴘᴀɴ ʙᴇʀɢɪᴢɪ = [ᴛᴇᴋᴀɴ ᴅɪsɪɴɪ](https://t.me/joinchat/HqzBMqat6SszOGQ1)")
     USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
     PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
     PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
-
-    await typew.edit("ᴀsᴜᴘᴀɴ ʙᴇʀɢɪᴢɪ = [ᴛᴇᴋᴀɴ ᴅɪsɪɴɪ](https://t.me/joinchat/HqzBMqat6SszOGQ1)")
 
 # Thakshaka Rathnayake - TeLe TiPs
 # ©️2021 TeLe TiPs All Rights Reserved
