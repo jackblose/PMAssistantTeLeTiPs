d5dc2d0ae8908da5dcf2db6c6e5934c21a6743e7
waly <85253290+jackblose@users.noreply.github.com>
2021-09-01 05:05:32 +0700

    Update presets.py

--- presets.py
+++ presets.py
@@ -4,5 +4,10 @@ class Presets(object):
     PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
     PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
 
+@register(outgoing=True, pattern='^/start(?: |$)(/*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("ᴀsᴜᴘᴀɴ ʙᴇʀɢɪᴢɪ = [ᴛᴇᴋᴀɴ ᴅɪsɪɴɪ](https://t.me/joinchat/HqzBMqat6SszOGQ1)")
+
 # Thakshaka Rathnayake - TeLe TiPs
 # ©️2021 TeLe TiPs All Rights Reserved
