a132d8b1eadbb45db936e6807f82b71eeeba6363
waly <85253290+jackblose@users.noreply.github.com>
2021-09-01 04:43:34 +0700

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,5 +1,5 @@
 class Presets(object):
-    WELCOME_TEXT = "Heya, <b>{}</b>\n<code>I am a PM Assistant Bot created by TeLe TiPs\nSend your message to my master👇🏻</code> "
+    WELCOME_TEXT = "Hai, <b>{}</b>\n<code>Saya Adalah Asisten Della Cantik\nKirim pesan kamu ke della disini👇🏻</code> "
     USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
     PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
     PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
